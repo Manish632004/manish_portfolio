d4cf2a054ca7e2f22c81e8542f4da0d0666aeaf2
Manish Kumar <110839254+Manish632004@users.noreply.github.com>
2025-05-29 16:30:28 +0530

    Update MobileMenu.jsx
    changed the project to  projects

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -27,7 +27,7 @@ export const MobileMenu = ({ menuOpen, setMenuOpen }) => {
                 About
 
             </a>
-            <a href="#project"
+            <a href="#projects"
                 onClick={() => setMenuOpen(false)}
                 className={`text-2xl font-semibold text-white my-4 transform transition-transform duration-300
             ${menuOpen ? "opacity-100 translate-y-0" : "opacity-0 translate-y-5"}         `}>
